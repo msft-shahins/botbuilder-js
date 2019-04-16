0138ba83cf8dd4d7f7d5b3df736afe80b7876d4f
Billy Delgado <37461749+Bill7zz@users.noreply.github.com>
2019-04-16 17:28:13 -0300

    Add functional-test script

---
+++ libraries/functional-tests/tests/azure.functional.test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+
+iniTest = function() {
+
+    it('Single Test', function() {
+        assert(true);
+    });
+};
+
+describe('Azure Functional Tests',function() {
+    iniTest();
+});
---
+++ libraries/functional-tests/tests/qna.functional.test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+
+iniTest = function() {
+
+    it('Single Test', function() {
+        assert(true);
+    });
+};
+
+describe('QnA Functional Tests',function() {
+    iniTest();
+});
